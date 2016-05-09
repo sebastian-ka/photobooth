48ead7b41f6ba09e34c9b76f647cbc454ea83edb
Sebastian <sebastian.kasenbacher@gmail.com>
2016-05-09 14:04:58 +0200

    threstrip working

--- magicktools.py
+++ magicktools.py
@@ -15,8 +15,8 @@ class Magicktools (object):
 				#self.createStripOne(filenames, s_width, s_height)
 			#if layout is Layout.Two :
 				#self.createStripTwo(filenames, s_width, s_height)
-			#if layout is Layout.Three :
-				#self.createStripThree(filenames, s_width, s_height)
+			if layout is Layout.Three :
+				self.createStripThree(filenames, s_width, s_height)
 			if layout is Layout.Four :
 				self.createStripFour(filenames, s_width, s_height)
 						
@@ -65,7 +65,43 @@ class Magicktools (object):
 		return {'left':left,'top':top}
 	
 	def createStripThree(self, filenames = ["img.png"], s_width = 400, s_height = 800):
-		return None
+		offset = 0.9
+		try:
+			with Color('white') as bg_c:
+				with Image(width = s_width, height = s_height, background= bg_c) as strip:
+					pos = 0
+					for fn in filenames:
+						print(fn)
+						try:
+							with Image(filename=fn) as pic:
+								with pic.clone() as pc :
+									pc_width = int(s_width)
+									pc_height = int(s_height /3)
+									pc.resize(int(pc_width * offset), int(pc_height * offset))
+									pc_pos = self.__getPositionThree(pos, s_width, s_height)
+									strip.composite(pc, left = pc_pos['left'],top = pc_pos['top'] )
+						except Exception, err:
+							print(err)
+						pos+=1
+					strip.save(filename="strip.png")
+		except Exception, err:
+			print(err)
+	
+	def __getPositionThree(self, pos, w, h):
+		left =0
+		top = 0
+		offset = 0.9
+		pic_w = int(w)
+		pic_h = int(h/3)
+		
+		if pos == 1 :
+			top = pic_h
+		if pos == 2 :
+			top = pic_h * 2
+		left += int((1-offset)*0.5*pic_w)
+		top  += int((1-offset)*0.3*pic_h)
+		return {'left':left,'top':top}
+		
 	
 	def createBackground(self, color = 'white', s_width = 400, s_height = 800):
 		try:
--- test.py
+++ test.py
@@ -3,6 +3,8 @@ from magicktools import Layout
 
 
 mt = Magicktools()
+filenames3 = ["img1.png", "img2.png","img3.png"]
 filenames = ["img1.png", "img2.png","img3.png","img4.png"]
-mt.createStrip(Layout.Four,filenames,400,800)
+#mt.createStrip(Layout.Four,filenames,400,800)
+mt.createStrip(Layout.Three,filenames3,400,800)
 #mt.createStripFour(300,600,filenames)
